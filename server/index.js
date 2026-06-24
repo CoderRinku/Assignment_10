bed89243878cd8d7a8d9a7cd268f57d16c0902ca
CoderRinku <roknusjamanrinku1@gmail.com>
2026-06-25 00:36:37 +0600

    Implement JWT generation and Verify Token middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
 import { MongoClient } from 'mongodb'
 
 dotenv.config()
@@ -14,11 +15,34 @@ app.use(express.json())
 const uri = process.env.DB_URI || 'mongodb://localhost:27017'
 const client = new MongoClient(uri)
 
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'Unauthorized access' })
+  }
+  const token = req.headers.authorization.split(' ')[1]
+  jwt.verify(token, process.env.JWT_SECRET || 'fallback_secret', (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
 async function run() {
   try {
     await client.connect()
     console.log('Successfully connected to MongoDB')
     
+    app.post('/jwt', (req, res) => {
+      const { email } = req.body
+      if (!email) {
+        return res.status(400).send({ message: 'Email is required' })
+      }
+      const token = jwt.sign({ email }, process.env.JWT_SECRET || 'fallback_secret', { expiresIn: '1h' })
+      res.send({ token })
+    })
+
     app.get('/', (req, res) => {
       res.send({ status: 'Server is running perfectly' })
     })
